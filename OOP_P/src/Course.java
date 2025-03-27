32985da7371a003794390fd22835f954fd5415bf
Ednah-World <ednah.mageria@strathmore.edu>
2025-03-28 01:12:36 +0300

    Main

--- OOP_P/src/Course.java
+++ OOP_P/src/Course.java
@@ -1,10 +1,10 @@
 public class Course {
     private String course_id;
     private String course_name;
-    private int credits;
+    private String credits;
     private String instructor;
 
-    public Course (String course_id,String course_name,int credits, String instructor){
+    public Course (){
         this.course_id = course_id;
         this.course_name = course_name;
         this.credits = credits;
