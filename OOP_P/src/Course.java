eb767883db9eb492dbf0f7cf6eb500b56b48e763
Ednah-World <ednah.mageria@strathmore.edu>
2025-03-28 01:15:40 +0300

    Revert "Main"
    This reverts commit 32985da7371a003794390fd22835f954fd5415bf.

--- OOP_P/src/Course.java
+++ OOP_P/src/Course.java
@@ -1,10 +1,10 @@
 public class Course {
     private String course_id;
     private String course_name;
-    private String credits;
+    private int credits;
     private String instructor;
 
-    public Course (){
+    public Course (String course_id,String course_name,int credits, String instructor){
         this.course_id = course_id;
         this.course_name = course_name;
         this.credits = credits;
--- OOP_P/src/Main.java
+++ OOP_P/src/Main.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         Statement state;
-        connectdb db = new connectdb();
-        Connection conn = db.conn("OOP_Projct", "postgres", "Wanjiru1806");
+        connectdb db= new connectdb();
+        Connection conn=db.conn("OOP_Projct","postgres","Wanjiru1806");
 
         try {
             String query = "insert into courses (course_id, course_name, credits, instructor) values ('001', 'BComm', '4','Prof. Wafula')";
-            state = conn.createStatement();
+            state= conn.createStatement();
             state.executeUpdate(query);
             System.out.println("Value inserted successfully");
         } catch (Exception e) {
@@ -21,8 +21,8 @@ public class Main {
         }
 
         try {
-            String query = "insert into Student (student_id, student_name, email, phone_number, password) values ('5001', 'Bing Bong', 'boing@gmail.com','0771209193','chinkololo%')";
-            state = conn.createStatement();
+            String query= "insert into Student (student_id, student_name, email, phone_number, password) values ('5001', 'Bing Bong', 'boing@gmail.com','0771209193','chinkololo%')";
+            state= conn.createStatement();
             state.executeUpdate(query);
             System.out.println("Value inserted successfully");
         } catch (Exception e) {
@@ -31,7 +31,7 @@ public class Main {
 
         try {
             String query = "insert into Enrollments (enrollment_id, student_id, grade) values ('50', '5001', '001','A')";
-            state = conn.createStatement();
+            state= conn.createStatement();
             state.executeUpdate(query);
             System.out.println("Value inserted successfully");
         } catch (Exception e) {
@@ -40,12 +40,19 @@ public class Main {
 
         try {
             String query = "insert into staff (staff_id, department,phone_number,password) values ('S001', 'Mathematics','0727308900','kairetu27*')";
-            state = conn.createStatement();
+            state= conn.createStatement();
             state.executeUpdate(query);
             System.out.println("Value inserted successfully");
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Please enter course name: ");
+        String course_name= scanner.nextLine();
+
+        Mainframe myFrame = new Mainframe();
+        myFrame.init();
 
     }
 }
