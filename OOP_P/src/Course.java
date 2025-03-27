9e540ce9dd3967faa7e37d2609bfbe7da8789cbc
Ednah-World <ednah.mageria@strathmore.edu>
2025-03-28 00:06:46 +0300

    Revert "Course registration form"
    This reverts commit a5a28532f6672b68caa2aaad37647f472d3a39ef.

--- OOP_P/src/Course.java
+++ OOP_P/src/Course.java
@@ -1,10 +1,10 @@
 public class Course {
-    public static String course_id;
-    public static String course_name;
-    public static int credits;
-    public static String instructor;
+    private String course_id;
+    private String course_name;
+    private int credits;
+    private String instructor;
 
-    public Course (){
+    public Course (String course_id,String course_name,int credits, String instructor){
         this.course_id = course_id;
         this.course_name = course_name;
         this.credits = credits;
